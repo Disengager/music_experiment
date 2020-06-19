aef980f47c253d0d2b30368fc6187743c6da6740
Disengager <disengageramv@gmail.com>
2020-06-19 18:37:20 +0500

    init project

--- js/main.js
+++ js/main.js
@@ -1,24 +1,188 @@
-var test = {
-    time: 0,
-    filter: {
-        "show-persent": {
-            show: false,
-        },
-        "show-right-response": {
-            show: false,
-        },
-        "show-countdown": {
-            show: false,
-        },
-        "show-progress": {
-            show: false,
-            fixed: false
-        },
-        "show-random" : {
-            show: false,
+var test_creater = {
+    test_id: 0, //0 - rotation, 1 - toav
+    index: -1,
+    time: 0, //время
+    timer_status: false,
+    current_list: [], //список текущих
+    rotation_list: ['manual_1', 'rot_1', 'manual_2'],
+    toav_list: ['toav_manual_1', '', ''],
+    right: '',
+    playlist_sort: 1,
+    playlist_index: 0,
+    audio_list: [],
+
+    init: function() {
+        this.set_list_by_id();
+        this.next();
+
+    },
+    set_list_by_id: function() {
+        this.current_list = this.test_id == 0? this.rotation_list : this.toav_list
+    },
+    set_to_playlist: function(button) {
+        if($(button).html() == '+') {
+            exsist = false;
+            audio_current = $('#' + $(button).attr('for'))
+            for (audio in this.audio_list) {
+                if($(this.audio_list[audio]).attr('id') == $(audio_current).attr('id'))
+                    exsist = true
+            }
+            if(!exsist)
+                this.audio_list.push(audio_current);
+
+            $(button).html('-')   
+
+            
+        } else {
+            exsist = false;
+            audio_current = $('#' + $(button).attr('for'))
+            for (audio in this.audio_list) {
+                if($(this.audio_list[audio]).attr('id') == $(audio_current).attr('id'))
+                    exsist = audio
+            }
+            if(exsist)
+                this.audio_list.splice(exsist , 1);
+            $(button).html('+')  
+        }
+        this.audi_block_render();
+    },
+    audi_block_render: function() {
+        this.audio_list_sort(this.playlist_sort);
+        $('.playlist').html('');
+        for (audio in this.audio_list) {
+            selector = $(this.audio_list[audio]).attr('id').split('player_')[1];
+            $('.playlist').append('<div class="playlist_item">' +  $('#' + selector).html() + '</div>')
         }
     },
-    timer_status: false,
+    next: function(answer) {
+        if(this.is_right(answer))
+            return false
+        this.in_next(answer);
+
+    },
+    in_next: function(answer) {
+        this.index++;
+        if(this.index > this.current_list.length-1) {
+            
+            if(!this.is_rotation())
+                return this.end()
+
+            //запуск тоав
+            this.load_following_test();
+        }
+        this.render()
+    },
+    start: function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#current").offset().top
+        }, 1100);
+
+        this.timer_enable();
+
+        $('.audio_list span').attr('class', 'play-button');
+
+        $( '.audio_list span' ).each(function( index ) {
+            document.getElementById('player_' + $(this).attr('id')).pause();
+        });
+
+        $( '.audio_list .plus_button' ).each(function( index ) {
+            $(this).css('display', 'none');
+        });
+
+        this.play_from_playlist();
+    },
+    end: function() {
+
+    },
+    is_toav: function() {
+        return this.test_id == 1;
+    },
+    is_rotation: function() {
+        return this.test_id == 0;   
+    },
+    is_right: function(answer) {
+        if(this.index == -1)
+            return false
+
+        if(this.current_list[this.index] == undefined)
+            return false
+
+        if(parseInt($('#' + this.current_list[this.index] + ' .right').html()) == 0)
+            return false
+
+        if(parseInt($('#' + this.current_list[this.index] + ' .right').html()) == answer) {
+            $('#current .module__title').html('Верно');
+            setTimeout('test_creater.in_next(' + answer + ')', 600);
+            return true
+        }
+        else {
+            $('#current .module__title').html('Не верно');
+            setTimeout('test_creater.in_next(' + answer + ')', 600);
+            return true
+        }
+    },
+    load_following_test: function() {
+        this.time = 0;
+        this.test_id++;
+        this.index = 0;
+        this.set_list_by_id();  
+    },
+    render: function() {
+        $('#current').html($('#' + this.current_list[this.index]).html());
+
+    },
+    play_form_music: function(button) {
+        if($(button).attr('class') == 'pause-button') {
+            document.getElementById('player_' + $(button).attr('id')).pause();
+            $(button).attr('class', 'play-button');
+            return true;
+        }
+
+        $('.audio_list span').attr('class', 'play-button');
+        $(button).attr('class', 'pause-button');
+        $( '.audio_list span' ).each(function( index ) {
+            document.getElementById('player_' + $(this).attr('id')).pause();
+        });
+        document.getElementById('player_' + $(button).attr('id')).play();
+    },
+    play_from_playlist: function() {
+        if(this.audio_list.length < 1)
+            return false
+
+        if(this.playlist_index == this.audio_list.length)
+            this.playlist_index = 0;
+
+        document.getElementById('player_playlist').setAttribute('src', this.audio_list[this.playlist_index].attr('src'));
+        document.getElementById('player_playlist').play();
+
+        this.playlist_index++;
+    },
+    audio_list_sort: function(sort_type) {
+        var sort_arr = [];
+
+        for (audio in this.audio_list) {
+            sort_arr.push(parseInt($(this.audio_list[audio]).attr('id').split('player_music_')[1]));           
+        }
+        if(sort_type == 1)
+            sort_arr.sort();
+        else
+            sort_arr.sort((a, b) => b - a);
+
+        result_arr = [];
+
+        for (number in sort_arr) {
+            for(audio in this.audio_list) {
+                if($(this.audio_list[audio]).attr('id') == 'player_music_' + sort_arr[number]) {
+                    result_arr.push(this.audio_list[audio]);
+                    break;
+
+                }
+            }
+        }
+
+        this.audio_list = result_arr;
+
+    },
     timer_enable: function() {
         this.timer_status = true;
         this.time = 0;
@@ -28,161 +192,8 @@ var test = {
         this.timer_status = false;
         return this.time;
     },
-    start: function() {
-      $([document.documentElement, document.body]).animate({
-            scrollTop: $("#test").offset().top
-        }, 1100);
-
-        var questions = $('#question-text').val(),
-            question_separator = $('#question-separate').val(),
-            right_marker = $('#right-marker').val();
-
-        questions = new Spliter(questions, question_separator, right_marker).separate();
-        this.questions = questions;
-
-        $('#test .module').css("display", "block");
-
-        let max_qestion = $('#question-count').val(),
-            random = {},
-            max = questions.length-1;
-
-        max_qestion = max_qestion <= questions.length ? max_qestion : questions.length;
-
-        $('#questions').html('');
-        $('#progress__table .module__content').html('');
-        var printer = new Printer('#stat').init();
-
-        for(i = 0; i < parseInt(max_qestion); i++) {
-            
-            
-            if(this.filter['show-random'].show) {
-                var random_number = randomInteger(0, max);
-                while(true) {
-                    if(random[random_number] != undefined) {
-                        random_number = randomInteger(0, max);
-                    }
-                    else {
-                        random[random_number] = true;
-                        break;
-                    }
-                }
-            } else {
-                var random_number = i;
-            }
-            
-            random[random_number] = true;
-            let html_question = '<div class="question__title">' +  questions[random_number].question + "</div>",
-                html_right = '',
-                html_option = '';
-
-            for(right in questions[random_number].right)
-                html_right += '<div class="question__right" id="question' + random_number + (right > 0?'_'+right:'') + '" style="display: none;">' + questions[random_number].right[right] + '</div>';    
-            
-            for(option in questions[random_number].options) 
-                html_option += '<div><input type="' + (questions[random_number].right.length > 1 ?'checkbox' : (questions[random_number].options.length == 1? 'text':'radio')) + '" name="question' + random_number + '" id="question' + random_number + '_radio' + option + '"data-id="' + random_number + '">' +  (questions[random_number].options.length > 1?'<label for=question' + random_number + '_radio' + option +  '>' + questions[random_number].options[option] + '</label>' : '') + '</div>'
-            
-            let html_options = '<div class="question__options">' + html_option + '</div>';    
-            $('#questions').append('<div class="question" id="question_' + random_number + '">' + html_question + html_options + html_right + '</div>');
-            $('#progress__table .module__content').append('<a class="module__item" href="#question_' + random_number  + '" id="progress__item_' + random_number + '">' + (parseInt(i)+1) + '</a>');
-        }
-        checkEventListener();
-        addFlexScroll();
-        this.timer_enable();
-        if(test.filter['show-progress'].show) 
-            printer.progress_bar();
-        
-    },
-    clear: function(type) {
-        $("#test input:" + type).each(function(id, item) {
-            $(item).parent().attr('class', '');
-            $('#' + $(item).attr('name')).css("display", "none");
-        });
-    },
-    check: function() {
-        var count = $('.question').length,
-            right_count = 0;
-
-        this.clear('radio');
-        this.clear('checkbox');
-        this.clear('text');
-
-        let test = this;
-
-        $("#test input:radio:checked").each(function(id, item){
-            let current = $(item).parent().find('label').html(),
-                question_id = $(item).data('id'),
-                right = test.questions[question_id].right[0];
-            
-            if(current == right) {
-                $(item).parent().attr('class', 'correct-answer');
-                $('#progress #progress__item_' + question_id).addClass('correct-question');
-                test.questions[question_id].result = 1;
-            }
-            else {
-                $(item).parent().attr('class', 'wrong-answer');
-                $('#progress #progress__item_' + question_id).addClass('wrong-question');
-                $('#' + $(item).attr('name')).css("display", "block");
-            }
-        });
-
-        $("#test input:checkbox:checked").each(function(id, item){
-            let current = $(item).parent().find('label').html(),
-                question_id = $(item).data('id'),
-                right_index = test.questions[question_id].right.indexOf( current );
-
-            if(right_index != -1) {
-                $(item).parent().attr('class', 'correct-answer');
-                $('#progress #progress__item_' + question_id).addClass('correct-question');
-                test.questions[question_id].result += 1 / test.questions[question_id].right.length;
-            }
-            else {
-                test.questions[question_id].result = -1;
-                $(item).parent().attr('class', 'wrong-answer');
-                $('#progress #progress__item_' + question_id).addClass('wrong-question');
-                $(item).parent().parent().parent().find('.question__right').css("display", "block");
-            }
-        });
-
-        $("#test input:text").each(function(id, item){
-            let current = $(item).val();
-            if( current == "" ) return false;
-
-            let question_id = $(item).data('id');
-            if(current.toLowerCase() == test.questions[question_id].right[0].toLowerCase()) {
-                $(item).parent().attr('class', 'correct-answer');
-                $('#progress #progress__item_' + question_id).addClass('correct-question');
-                test.questions[question_id].result = 1
-            }
-            else {
-                $(item).parent().attr('class', 'wrong-answer');
-                $('#progress #progress__item_' + question_id).addClass('wrong-question');
-                $(item).parent().parent().parent().find('.question__right').css("display", "block");
-            }
-        });
-
-        for(question in test.questions) 
-            right_count += test.questions[question].get_result();
-
-        var printer = new Printer('#stat').init(),
-            time = this.time;
-
-        if(this.filter['show-persent'].show) {
-            let percent_right = right_count * 100 / count,
-                response = '<br><br><div><span class="correct-answer">Правильных ответов: ' + parseInt(percent_right) + '% (' + right_count + ')</span><br><br><br><span class="wrong-answer">Неправельных ответов: ' + parseInt(100-percent_right) + '% (' + (count-right_count) + ')</span></div><br><br><br>'    
-            printer.set(response);
-        }
-
-        if(this.filter['show-countdown'].show) {
-            let hour = parseInt(time / 3600),
-                minute = parseInt(time / 60) - (hour * 60),
-                second = time - (minute * 60) - (hour * 3600),
-                response = '<b>Вы прошли тест за: </b>' + (hour > 0? hour + ' ' + num2str(hour, ['час', 'час', 'часов']) + ' ' : '') + (minute > 0? minute + ' ' + num2str(minute, ['минута', 'минуты', 'минут']) + ' ' : '') + (second > 0? second + ' ' + num2str(second, ['секунда', 'секунды', 'секунд']) + ' ' : '');
-            printer.set(response);
-        }
-
-    },
     stop: function() {
-        this.check();
+        // this.check();
         this.timer_disable();
         // new Printer('#stat').progress_bar_stop();
     },
@@ -194,5 +205,33 @@ var test = {
             me.timer();
         }, 1000);
     },
-
 }
+
+                                                                                                                                                                                                                                       
+document.onkeydown = function(e) {
+    switch(e.which) {
+        case 37: // left
+            test_creater.next(1)
+        break;
+
+        case 39: // right
+            test_creater.next(2);
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); 
+};
+
+
+function pause (ms) {
+    if (ms<=0) return;
+    var now = new Date();
+    now.setTime (now.getTime()+ms);
+    var end=now.getTime();
+    do {
+    now = new Date();
+    } while (now.getTime()<end);
+}
+
+
